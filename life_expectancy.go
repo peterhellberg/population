e3241fb1aff7fb5cf6c8fc456545cc010bcf5189
Peter Hellberg <peter@c7.se>
2015-10-04 16:35:27 +0200

    Added RemainingLifeExpectancy

---
+++ life_expectancy.go
@@ -0,0 +1,34 @@
+package population
+
+import (
+	"fmt"
+	"time"
+)
+
+// RemainingLifeExpectancy contains remaining life expectancy of
+// a person with given sex, country, and age at a given point in time
+type RemainingLifeExpectancy struct {
+	Sex       string  `json:"sex"`
+	Country   string  `json:"country"`
+	Date      string  `json:"date"`
+	Age       string  `json:"age"`
+	Remaining float64 `json:"remaining_life_expectancy"`
+}
+
+// RemainingLifeExpectancy calculates remaining life expectancy of a
+// person with given sex, country, and age at a given point in time
+//
+// <http://api.population.io/#!/life-expectancy/calculateRemainingLifeExpectancy>
+//
+func (c *Client) RemainingLifeExpectancy(sex, country, date, age string) (RemainingLifeExpectancy, error) {
+	path := fmt.Sprintf(`/life-expectancy/remaining/%s/%s/%s/%s/`, sex, country, date, age)
+	resp := RemainingLifeExpectancy{}
+
+	if _, err := time.Parse("2006-01-02", date); err != nil {
+		return resp, err
+	}
+
+	err := c.Get(path, &resp)
+
+	return resp, err
+}
---
+++ life_expectancy_test.go
@@ -0,0 +1,72 @@
+package population
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+)
+
+func TestRemainingLifeExpectancy(t *testing.T) {
+	for i, tt := range []struct {
+		path      string
+		sex       string
+		country   string
+		date      string
+		age       string
+		remaining float64
+	}{
+		{
+			"/life-expectancy/remaining/male/United Kingdom/2001-05-11/49y2m/",
+			"male",
+			"United Kingdom",
+			"2001-05-11",
+			"49y2m",
+			32.63468564999831,
+		},
+	} {
+		ts, c := testServerAndClient(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+
+			if r.URL.Path != tt.path {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(`{"detail":"bad request"}`))
+				return
+			}
+
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"sex":     tt.sex,
+				"country": tt.country,
+				"date":    tt.date,
+				"age":     tt.age,
+				"remaining_life_expectancy": tt.remaining,
+			})
+		})
+
+		resp, err := c.RemainingLifeExpectancy(tt.sex, tt.country, tt.date, tt.age)
+		ts.Close()
+
+		if err != nil {
+			t.Fatalf(`[%d] unexpected error: %v`, i, err)
+		}
+
+		if resp.Sex != tt.sex {
+			t.Fatalf(`[%d] resp.Sex = %q, want %q`, i, resp.Sex, tt.sex)
+		}
+
+		if resp.Country != tt.country {
+			t.Fatalf(`[%d] resp.Country = %q, want %q`, i, resp.Country, tt.country)
+		}
+
+		if resp.Date != tt.date {
+			t.Fatalf(`[%d] resp.Date = %q, want %q`, i, resp.Date, tt.date)
+		}
+
+		if resp.Age != tt.age {
+			t.Fatalf(`[%d] resp.Age = %q, want %q`, i, resp.Age, tt.age)
+		}
+
+		if resp.Remaining != tt.remaining {
+			t.Fatalf(`[%d] resp.Remaining = %v, want %v`, i, resp.Remaining, tt.remaining)
+		}
+	}
+}
